ebfc3e5ecafc59f08f6ab9e4d6a06924be314397
Karthik T <karthikt.holmes+github@gmail.com>
2014-09-28 16:12:50 +0800

    Some more dependencies

--- installdeps.sh
+++ installdeps.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo apt-get install libx11-dev #for tk
-cpan -i DBI DBD::CSV File::Spec::Functions SQL::Interp Tk Sort::Naturally File::Find::Rule
+sudo apt-get install libx11-dev libpng12-dev #for tk
+cpan -i YAML DBI DBD::CSV File::Spec::Functions SQL::Interp Tk Sort::Naturally File::Find::Rule Switch
